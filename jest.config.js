cbe884abea32700fd365d9910dd22ae70062aa7e
Kyungil Park <kylepark77@gmail.com>
2019-07-27 23:08:54 +0900

    feat(#119): 예제 TC 작성

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,13 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-  collectCoverageFrom: [
-      "workspace/js_src/**/*.js"
-  ],
-  coverageDirectory: "report/coverage",
-  testEnvironment: "node"
+    collectCoverageFrom: [
+        "workspace/js_src/**/*.js"
+    ], 
+    coverageDirectory: "report/coverage",
+    browser: true,
+    transformIgnorePatterns: [
+        "/node_modules/",
+        "/lib/"
+    ]
 };
---
+++ workspace/test/util/hp_SE2M_Utils.test.js
@@ -0,0 +1,26 @@
+require("../../../dist/js/lib/jindo2.all");
+require("../../js_src/util/hp_SE2M_Utils");
+
+describe("StringBuffer", () => {
+    it("toString > 저장된 문자열을 반환한다.", () => {
+        // given
+        const sb = new StringBuffer("hello");
+
+        // when
+        const str = sb.toString();
+
+        // then
+        expect(str).toEqual("hello");
+    });
+
+    it("append > 문자열을 덧붙인다.", () => {
+        // given
+        const sb = new StringBuffer("hello");
+
+        // when
+        sb.append(" ").append("world");
+
+        // then
+        expect(sb.toString()).toEqual("hello world");
+    });
+});
